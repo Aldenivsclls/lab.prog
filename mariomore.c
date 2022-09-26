11938a03b0f3071c5e53efa8772133cf92a374b6
Aldenivsclls <62741745+Aldenivsclls@users.noreply.github.com>
2022-09-26 14:42:47 -0300

    Add files via upload

---
+++ mariomore.c
@@ -0,0 +1,27 @@
+//questao 2
+//mario more
+#include <stdio.h>
+#include <cs50.h>
+
+int main () {
+
+    int alt;
+    do {
+    alt= get_int ("Altura: ");
+    }
+    while (alt < 1 || alt > 8);
+
+    for (int lin = 0; lin < alt; lin++) {
+        for (int esp = 0; esp < alt - lin - 1; esp++){
+            printf(" ");
+        }
+        for(int col =0; col <= lin; col++) {
+        printf("#");
+        }
+        printf("   ");
+        for (int col = 0; col <= lin; col++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
